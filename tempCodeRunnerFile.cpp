06f001d93730d0a516276b4a6cecd187a23c2a82
sam <samadhanbhusnar@gmail.com>
2022-10-21 17:37:24 +0530

    An new repository

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+t << "this is int :  " << sizeof(int) << endl;
+    cout << "this is float :  " << sizeof(float) << endl;
+    cout << "this is double :  " << sizeof(double) << endl;
+    cout << "this is bool :  " << sizeof(bool) << endl;
+    cout << "this is char :  " << sizeof(char) << endl;
+    cout << "this is unsignint :  " << sizeof(unsigned) << endl;
+    cout << "this is signint :  " << sizeof(signed) << endl;
+    cout << "Sangola\n";
